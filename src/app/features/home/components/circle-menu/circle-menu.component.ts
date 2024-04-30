d32762eaa13844846386969655c93c861556fd1e
FaridFehresti <115652523+FaridFehresti@users.noreply.github.com>
2024-05-01 00:36:25 +0330

    fixed the issue with circle and radial background

--- src/app/features/home/components/circle-menu/circle-menu.component.ts
+++ src/app/features/home/components/circle-menu/circle-menu.component.ts
@@ -11,34 +11,8 @@ export class CircleMenuComponent {
   radius: number = 200; // Adjust this value to set the maximum distance from the center
 
   updateCoordinates(event: MouseEvent) {
-    const container = event.currentTarget as HTMLElement;
-    const containerRect = container.getBoundingClientRect();
-    const containerX = containerRect.left;
-    const containerY = containerRect.top;
-
-    const mouseX = event.clientX;
-    const mouseY = event.clientY;
-
-    const relativeX = mouseX - containerX;
-    const relativeY = mouseY - containerY;
-
-    const centerX = containerRect.width / 2;
-    const centerY = containerRect.height / 2;
-
-    const distanceX = relativeX - centerX;
-    const distanceY = relativeY - centerY;
-
-    const distance = Math.sqrt(distanceX ** 2 + distanceY ** 2);
-
-    if (distance <= this.radius) {
-      this.x = relativeX + 'px';
-      this.y = relativeY + 'px';
-    } else {
-      const angle = Math.atan2(distanceY, distanceX);
-      const maxDistanceX = Math.cos(angle) * this.radius;
-      const maxDistanceY = Math.sin(angle) * this.radius;
-      this.x = centerX + maxDistanceX + 'px';
-      this.y = centerY + maxDistanceY + 'px';
-    }
+    const target = event.currentTarget as HTMLElement;
+    this.x = event.pageX - target.offsetLeft + 'px';
+    this.y = event.pageY - target.offsetTop + 'px';
   }
 }
